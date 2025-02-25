d514b01cdf7b058256593473840ff4085f47ed72
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-02-26 01:08:17 +0500

    exploring get, post and delete method

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Mr. Abdullah Manan");
+// This will only handle GET call
+app.get("/user", (req, res) => {
+  res.send({ fName: "Abdullah", lname: "Manan" });
 });
 
-app.get("/abdullah", (req, res) => {
-  res.send("Hello Mr. Abdullah Manan");
+// This will only handle POST call
+app.post("/user", (req, res) => {
+  res.send("Data successfully send to database!");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("Hello Hello Hello Hello...");
+// This will only handle DELETE call
+app.delete("/user", (req, res) => {
+  res.send("Data successfully deleted!");
+});
+
+// This will  handle all call
+app.use("/test", (req, res) => {
+  res.send("Hello from server");
 });
 
 app.listen(3000, () => {

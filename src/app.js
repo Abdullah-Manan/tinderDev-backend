0e99761444a28be26d6d80fa10a45c9d328cdef9
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-04-24 00:25:56 +0500

    New Changes

--- src/app.js
+++ src/app.js
@@ -1,24 +1,8 @@
 const express = require("express");
 const app = express();
 
-// This will only handle GET call
-app.get("/user", (req, res) => {
-  res.send({ fName: "Abdullah", lname: "Manan" });
-});
-
-// This will only handle POST call
-app.post("/user", (req, res) => {
-  res.send("Data successfully send to database!");
-});
-
-// This will only handle DELETE call
-app.delete("/user", (req, res) => {
-  res.send("Data successfully deleted!");
-});
-
-// This will  handle all call
-app.use("/test", (req, res) => {
-  res.send("Hello from server");
+app.use("/user", (req, res) => {
+  res.send("User Detail");
 });
 
 app.listen(3000, () => {

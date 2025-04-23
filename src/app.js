9f22f6dff0724b87bce65922f76de7d3584aeabc
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-04-24 00:31:24 +0500

    new changes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send("User Detail");
-});
+app.use(
+  "/user",
+  (req, res) => {
+    res.send("1st User Detail");
+  },
+  (req, res) => {
+    res.send("2nd User Detail");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");

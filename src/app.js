257c1e789d2bad5e06546275edc77f28e70f574b
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-04-27 21:55:31 +0500

    database

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 const express = require("express");
+require("./config/database.js");
 const { auth, auth2 } = require("./middleware/auth.js");
 const app = express();
-
-app.use("/user", auth);
-
-app.get("/user", (req, res) => {
-  res.send("1st User Detail");
-});
-
-app.get("/user2", auth2, (req, res) => {
-  res.send("2nd User Detail");
-});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");

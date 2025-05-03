4c82272efbc6fd8a62cdcc1a260011032bf8885b
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-05-04 02:41:19 +0500

    Implement footer

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 require("./config/database.js");
 const { auth, auth2 } = require("./middleware/auth.js");
+const { auth, auth2 } = require("./middleware/auth.js");
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000");
   console.log("Server is successfully listening on port 3000");
 });

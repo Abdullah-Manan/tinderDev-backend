a69910b4b19a6db46888a2a38b265b7d89ed172e
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-05-09 23:38:39 +0500

    Implement footer

--- src/app.js
+++ src/app.js
@@ -5,5 +5,4 @@ const app = express();
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000");
-  console.log("Server is successfully listening on port 3000");
 });

c40b55506ffddd1fcd0e5da0dc774f7d69e146b8
Abdullah-Manan <abdullahmanan010@gmail.com>
2025-05-27 20:57:20 +0500

    CRUD operation

--- src/app.js
+++ src/app.js
@@ -4,6 +4,53 @@ const app = express();
 const User = require("./model/userschema");
 app.use(express.json());
 
+app.post("/signin", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ message: "User saved successfully", user });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: "Error saving user", details: error.message });
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const user = await User.find({ email: userEmail });
+    res.status(201).json({ message: "User", user });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: "Error saving user", details: error.message });
+  }
+});
+
+app.put("/update-user", async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const user = await User.find({ email: userEmail });
+    res.status(201).json({ message: "User", user });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: "Error saving user", details: error.message });
+  }
+});
+app.delete("/delete-user", async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const user = await User.find({ email: userEmail });
+    res.status(201).json({ message: "User", user });
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error: "Error saving user", details: error.message });
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("Datbase is connected");
@@ -14,25 +61,3 @@ connectDB()
   .catch(() => {
     console.log("Datbase is not connected");
   });
-
-app.post("/signin", async (req, res) => {
-  // console.log("Body",req.body);
-  try {
-    // const { firstName, lastName, email, age, password, type } = req.body;
-    // const data = {
-    //   firstName:"Abdullah",
-    //   lastName:"Manan",
-    //   email:"abdullahmanan@gmail.com",
-    //   age:20,
-    //   password:"123456",
-    //   gender:"male",
-    // };
-    const user = new User(req.body);
-    await user.save();
-    res.status(201).json({ message: "User saved successfully", user });
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "Error saving user", details: error.message });
-  }
-});

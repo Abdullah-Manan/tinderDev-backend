921eedea61ec728d38f5a3278e285ce386a36007
mimranx0 <abdullahmanan010@gmail.com>
2025-08-21 21:34:14 +0500

    create feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,10 +3,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../model/connectionRequest");
+const User = require("../model/userschema");
 
 const userRouter = express.Router();
 
-const allowData = ["firstName", "lastName", "email", "photoUrl"];
+const allowData = ["firstName", "lastName", "email", "photoUrl", "skills"];
 
 // get all the connection requests received by the logged in user
 userRouter.get("/user/request/received", userAuth, async (req, res) => {
@@ -41,7 +42,6 @@ userRouter.get("/user/request/received", userAuth, async (req, res) => {
 });
 
 // get all the connections of the logged in user
-// this will return all the users that the logged in user is connected to
 userRouter.get("/user/connections", userAuth, async (req, res) => {
   try {
     if (!req.user || !req.user._id) {
@@ -85,6 +85,48 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Failed to fetch connections.",
+    });
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const page = req.query.page || 1;
+    const pageSize = req.query.pagesize || 2;
+    const skip = (page - 1) * pageSize;
+
+    const connectionSet = new Set();
+
+    const connectionWithUser = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUserId }, { toUserId: loggedInUserId }],
+    }).select("fromUserId toUserId");
+
+    for (const connection of connectionWithUser) {
+      connectionSet.add(connection.fromUserId);
+      connectionSet.add(connection.toUserId);
+    }
+
+    const feed = await User.find({
+      _id: {
+        $nin: Array.from(connectionSet),
+        $ne: loggedInUserId,
+      },
+    })
+      .skip(skip)
+      .limit(pageSize)
+      .select(allowData);
+
+    return res.json({
+      success: true,
+      message: "Feed fetched successfully.",
+      data: feed,
+    });
+  } catch (error) {
+    console.error("Error fetching Feed:", error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch feed.",
     });
   }
 });

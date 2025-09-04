8f4e80670fdbf00cca350ebb6e591be3d4421af4
Abdullah Manan <abdullahmanan010@gmail.com>
2025-09-04 12:56:43 +0500

    add cronjobs

--- src/utils/cronjobs.js
+++ src/utils/cronjobs.js
@@ -1,7 +1,33 @@
 const cron = require("node-cron");
+const { subDays, startOfDay, endOfDay } = require("date-fns");
+const ConnectionRequestModel = require("../model/connectionRequest");
+const sendEmail = require("./sendEmail");
 
-cron.schedule("* * * * *", () => {
-  console.log("Cron job running" + new Date());
+cron.schedule("31 12 * * *", async () => {
+  try {
+    const yesterday = subDays(new Date(), 1);
+    const yeasterdatStart = startOfDay(yesterday);
+    const yeasterdatEnd = endOfDay(yesterday);
+
+    const pendingRequests = await ConnectionRequestModel.find({
+      status: "interested",
+      createdAt: { $gte: yeasterdatStart, $lte: yeasterdatEnd },
+    }).populate("fromUserId toUserId");
+
+    const listOfUsers = [
+      ...new Set(pendingRequests.map((request) => request.fromUserId.email)),
+    ];
+
+    for (const user of listOfUsers) {
+      const emailResponse = await sendEmail.run(
+        "You have a new connection request",
+        "You have a new connection request" + user
+      );
+      console.log(emailResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = { cron };
